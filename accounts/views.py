914999a24bc45242350f932181d0cc5db9efddc9
bobfaith <chettri.babin@gmail.com>
2022-03-01 16:52:18 +0530

    add signals file and modify registrations function.

--- accounts/views.py
+++ accounts/views.py
@@ -27,9 +27,8 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,)
+
+
             messages.success(request, 'Account created for ' + username)
             return redirect('login')
 
